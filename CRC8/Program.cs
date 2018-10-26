34dce426a913bc110d8d900882ef4581ba9aedaf
Alexander Pak <pak@mercury.office>
2018-10-26 13:12:19 +0400

    init commit

---
+++ CRC8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CRC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new FileStream("input.txt", FileMode.Open))
+            {
+                byte result = CRC8.Calculate(stream);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
